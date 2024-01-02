b5076f1430a6e464d159e233ab3b27e2a3997ef4
Pawan <pawanjbs5@gmail.com>
2024-01-02 10:19:37 +0530

    telescope findfiles or git files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -15,17 +15,6 @@ return {
 				end
 			},
 		},
-		keys = {
-
-			{ '<leader>?',       "<cmd>Telescope oldfiles<cr>",            { desc = '[?] Find recently opened files' } },
-			{ '<leader><space>', "<cmd>Telescope buffers<cr>",             { desc = '[ ] Find existing buffers' } },
-			{ '<leader>gf',      "<cmd>Telescope git_files<cr>",           { desc = 'Search [G]it [F]iles' } },
-			{ '<leader>ff',      "<cmd>Telescope find_files<cr>",          { desc = '[S]earch [F]iles' } },
-			{ '<leader>sh',      "<cmd>Telescope help_tags<cr>",           { desc = '[S]earch [H]elp' } },
-			{ '<leader>sw',      "<cmd>Telescope grep_string<cr>",         { desc = '[S]earch current [W]ord' } },
-			{ '<leader>sg',      "<cmd>Telescope live_grep<cr>",           { desc = '[S]earch by [G]rep' } },
-			{ '<leader>sd',      "<cmd>Telescope diagnostics bufnr=0<cr>", { desc = '[S]earch [D]iagnostics' } },
-		},
 		opts = {
 			defaults = {
 				mappings = {
@@ -35,6 +24,23 @@ return {
 					},
 				},
 			},
+		},
+		keys = {
+
+			{ '<leader>?',       "<cmd>Telescope oldfiles<cr>", { desc = '[?] Find recently opened files' } },
+			{ '<leader><space>', "<cmd>Telescope buffers<cr>",  { desc = '[ ] Find existing buffers' } },
+			{ '<leader>ff', function()
+				local builtin = require("telescope.builtin")
+				if vim.loop.fs_stat(vim.loop.cwd() .. "/.git") then
+					builtin.git_files({ show_untracked = true })
+				else
+					builtin.find_files()
+				end
+			end, { desc = '[S]earch [F]iles' } },
+			{ '<leader>sh', "<cmd>Telescope help_tags<cr>",           { desc = '[S]earch [H]elp' } },
+			{ '<leader>sw', "<cmd>Telescope grep_string<cr>",         { desc = '[S]earch current [W]ord' } },
+			{ '<leader>sg', "<cmd>Telescope live_grep<cr>",           { desc = '[S]earch by [G]rep' } },
+			{ '<leader>sd', "<cmd>Telescope diagnostics bufnr=0<cr>", { desc = '[S]earch [D]iagnostics' } },
 		},
 	},
 }

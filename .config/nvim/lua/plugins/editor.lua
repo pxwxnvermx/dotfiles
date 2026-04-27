d3bc70b843344de9fad78b5cf5f6639cfebbd413
Pawan Verma <pawanjbs5@gmail.com>
2026-04-27 18:26:01 +0530

    Add formatting

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -89,4 +89,14 @@ return {
 			indent = { enable = true, disable = { 'ruby' } },
 		},
 	},
+	{
+		'stevearc/conform.nvim',
+		event = { 'BufWritePre' },
+		cmd = { 'ConformInfo' },
+		opts = {
+			notify_on_error = false,
+			format_on_save = { timeout_ms = 500, lsp_format = 'fallback', },
+			formatters_by_ft = {},
+		},
+	}
 }
